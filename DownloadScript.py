3b72692548c454901d51b19897b2614d12af1839
aleks-dimoski <aleksdimoski890@gmail.com>
2021-02-03 14:53:47 -0500

    bug fixes and automated image cleaning

--- DownloadScript.py
+++ DownloadScript.py
@@ -1,7 +1,10 @@
 import os
+import ImageCleaner
 from google_images_download import google_images_download
 
 ### https://google-images-download.readthedocs.io/en/latest/examples.html ###
+
+# Be sure to mount any external drives with 'sudo fdisk -l' and then 'sudo mount -t ntfs /dev/sda1 /media/aleks' or similar
 
 xit = str(input("This code will download based on previous arguments. Modify the python file to change download args. Continue? (y/n) "))
 if not xit == "y":
@@ -10,7 +13,7 @@ if not xit == "y":
 
 args = {"keywords": "painting",
         "suffix_keywords": "picasso,monet,leonardo da vinci,michelangelo,rembrandt,jmw turner,van gogh,mary cassatt",
-        "format": 'jpg',
+        "format": "jpg",
         "related_images": True,
         "limit": 500,
         "output_directory": "/media/aleks/DATA/Storage/Technical/Linux Resources/Images/ArtGen",
@@ -20,4 +23,5 @@ args = {"keywords": "painting",
 
 response = google_images_download.googleimagesdownload()
 absolute_image_paths = response.download(args)
-#print(absolute_image_paths)
+
+ImageCleaner.clean_images()
--- ImageCleaner.py
+++ ImageCleaner.py
@@ -4,7 +4,6 @@ import time
 from PIL import Image
 
 directory = "/media/aleks/DATA/Storage/Technical/Linux Resources/Images/ArtGen"
-count = 0
 
 def img_mod(image, index):
     # Image modification specifications #
@@ -17,19 +16,30 @@ def img_mod(image, index):
     layer = Image.new('RGB', (wd, ht), (0, 0, 0))
     img = img.resize((wd, ht))
     layer.paste(img)
-    img.save(directory+'/'+"image_num_"+index)
+    img.save(directory+'/'+image[:image.find('/')]+"/image_num_"+str(index)+'.png')
     time.sleep(0.02)
     img.close()
     os.remove(directory+'/'+image)
 
-image_paths = []
-for dir in os.listdir("/media/aleks/DATA/Storage/Technical/Linux Resources/Images/ArtGen"):
-    names = [(dir+'/'+x) for x in os.listdir(directory+'/'+dir) if x.endswith(".jpg") or x.endswith((".png"))]
-    image_paths += names
-print(len(image_paths))
+def clean_images():
+    count = 0
+    image_paths = []
+    invalid_paths = []
+    for dir in os.listdir("/media/aleks/DATA/Storage/Technical/Linux Resources/Images/ArtGen"):
+        names = [(dir+'/'+x) for x in os.listdir(directory+'/'+dir) if x.endswith(".jpg")]
+        invalid_names = [(dir+'/'+x) for x in os.listdir(directory+'/'+dir) if not x.endswith(".jpg")]
+        image_paths += names
+        invalid_paths += invalid_names
+    print(len(image_paths))
+    print(len(invalid_paths))
 
-for image in image_paths:
-    if count % 100 == 0:
-        print(count)
-    img_mod(image, count)
-    count += 1
+    for image in image_paths:
+        if count % 100 == 0:
+            print(count)
+        img_mod(image, count)
+        count += 1
+
+    for image in invalid_paths:
+        os.remove(directory+'/'+image)
+
+clean_images()
