2d15cd4c54367a6c6111cad15a410e1974d2b391
aleks-dimoski <aleksdimoski890@gmail.com>
2021-02-03 13:41:49 -0500

    standardized naming scheme

--- ImageCleaner.py
+++ ImageCleaner.py
@@ -1,13 +1,12 @@
 import os
 import time
-import re
 
 from PIL import Image
 
 directory = "/media/aleks/DATA/Storage/Technical/Linux Resources/Images/ArtGen"
 count = 0
 
-def img_mod(image):
+def img_mod(image, index):
     # Image modification specifications #
     wd = 256
     ht = 256
@@ -18,20 +17,19 @@ def img_mod(image):
     layer = Image.new('RGB', (wd, ht), (0, 0, 0))
     img = img.resize((wd, ht))
     layer.paste(img)
-    img.save(directory+'/'+image)
+    img.save(directory+'/'+"image_num_"+index)
     time.sleep(0.02)
     img.close()
-'''
+    os.remove(directory+'/'+image)
+
 image_paths = []
 for dir in os.listdir("/media/aleks/DATA/Storage/Technical/Linux Resources/Images/ArtGen"):
-    names = [(dir+'/'+x) for x in os.listdir(directory+'/'+dir) if x.endswith(".jpg")]
+    names = [(dir+'/'+x) for x in os.listdir(directory+'/'+dir) if x.endswith(".jpg") or x.endswith((".png"))]
     image_paths += names
 print(len(image_paths))
 
 for image in image_paths:
     if count % 100 == 0:
         print(count)
-    img_mod(image)
-    count += 1'''
-
-print(re.sub(r'\d*\.', r'_', '90.moz1.art'))
+    img_mod(image, count)
+    count += 1
